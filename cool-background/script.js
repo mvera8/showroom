5331995f9cbbb44db9488b2f596ca3bacef5cd51
Martin Vera <tinchovera8@hotmail.com>
2024-01-27 15:18:40 -0300

    Card Hover Effect 2

--- cool-background/script.js
+++
@@ -1,6 +0,0 @@
-function toggleTheme() {
-    if (document.body.classList.contains("dark"))
-        document.body.classList.remove("dark");
-    else
-        document.body.classList.add("dark");
-}
